bd23c7decd331889decd552d15e15e93f309b0ed
Tim Schlechter <timschlechter@gmail.com>
2013-07-19 22:09:00 +0200

    Support getting the value. Made sure only certain functions can be invoked.

--- js/bootstrap-tagsinput.js
+++ js/bootstrap-tagsinput.js
@@ -20,25 +20,35 @@
 	TagsInput.prototype = {
 		constructor: TagsInput,
 
-		add: function(item) {
+		addItem: function(item) {
 			var $tag = $(tagTemplate);
 			$(".text", $tag).text(item);
 			$tag.data('item', item);
 
 			$('input', this.$container).before($tag);
+
+			this.$element.val(this.getValueFromTags());
 		},
 
-		remove: function(item) {
+		removeItem: function(item) {
 			$('.tag', this.$container).filter(function(index) {
 				return $(this).data('item') === item;
 			}).remove();
+		},
+
+		getValueFromTags: function() {
+			return $.map($('.tag', this.$container), function(tag) { return $(tag).data('item'); });
+		},
+
+		getValueFromElement: function() {
+			return this.$element.val();
 		},
 
 		build: function() {
 			this.$container.on('keypress', 'input', $.proxy(function(event) {
 				var $input = $(event.target);
 				if(event.which == '13') {
-					this.add($input.val());
+					this.addItem($input.val());
 					$input.val('');
 				}
 
@@ -48,7 +58,7 @@
 			// Remove icon clicked
 			this.$container.on('click', '[data-role=remove]', $.proxy(function(event) {
 				var $tag = $(event.target).closest('.tag');
-				this.remove($tag.data('item'));
+				this.removeItem($tag.data('item'));
 			}, this));
 		},
 
@@ -63,21 +73,52 @@
 	};
 
 	// Register as a JQuery plugin
-	$.fn.tagsinput = function(option, parameter) {
-		return this.each(function() {
-			var data = $(this).data('tagsinput'),
+	$.fn.tagsinput = function(option, arg) {
+		var results = [];
+
+		this.each(function() {
+			var tagsinput = $(this).data('tagsinput'),
 				options = typeof option == 'object' && option;
 
-			// Initialize the tagsinput.
-			if (!data) {
-				$(this).data('tagsinput', ( data = new TagsInput(this, options)));
+			// Initialize a new tags input
+			if (!tagsinput) {
+				$(this).data('tagsinput', ( tagsinput = new TagsInput(this, options)));
+				results.push(tagsinput);
+				return;
 			}
 
-			// Call tagsinput method.
-			if ( typeof option == 'string') {
-				data[option](parameter);
+			// Invoke function on existing tags input
+			var retVal;
+			switch (option) {
+				case 'add':
+					retVal = tagsinput.addItem(arg);
+					break;
+				case 'remove':
+					retVal = tagsinput.removeItem(arg);
+					break;
+				case 'destroy':
+					retVal = tagsinput.destroy();
+					break;
+				case 'value':
+					if (arg)
+						retVal = tagsinput.setValue(arg);
+					else
+						retVal = tagsinput.getValueFromElement();
+					break;
+				default:
+					break;
 			}
+
+			if (retVal !== undefined)
+				results.push(retVal);
 		});
+
+		if ( typeof option == 'string') {
+			// Return the results from the invoked function calls
+			return results.length > 1 ? results : results[0];
+		} else {
+			return results;
+		}
 	};
 
 	$.fn.tagsinput.Constructor = TagsInput;
