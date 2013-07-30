46db372bd4ef9de65bdb69b3d78f459ed6d10aa5
Tim Schlechter <timschlechter@gmail.com>
2013-07-30 22:01:55 +0200

    Fixed two way binding bugs in angular directive

--- src/bootstrap-tagsinput-angular.js
+++ src/bootstrap-tagsinput-angular.js
@@ -6,17 +6,44 @@ angular.module('bootstrap.tagsinput', [])
       model: '=ngModel'
     },
     template: '<select multiple></select>',
-    replace: true,
+    replace: false,
     link: function(scope, element, attrs) {
       $(function() {
-        element.tagsinput({
+        if (!angular.isArray(scope.model))
+          scope.model = [];
+
+        var select = $('select', element);
+
+        select.tagsinput({
           source: angular.isFunction(scope.$parent[attrs.source]) ? scope.$parent[attrs.source] : null,
           itemValue: angular.isFunction(scope.$parent[attrs.itemvalue]) ? scope.$parent[attrs.itemvalue] : function(item) { return item[attrs.itemvalue]; },
           itemText: angular.isFunction(scope.$parent[attrs.itemtext]) ? scope.$parent[attrs.itemtext] : function(item) { return item[attrs.itemtext]; },
-          tagClass: angular.isFunction(scope.$parent[attrs.tagclass]) ? scope.$parent[attrs.tagclass] : function(item) { return attrs.tagclass; }
+          tagClass: angular.isFunction(scope.$parent[attrs.tagclass]) ? scope.$parent[attrs.tagclass] : function(item) { return attrs.tagclass; },
+          items : scope.model
         });
 
-        element.val(scope.model);
+        for (var i = 0; i < scope.model.length; i++) {
+          select.tagsinput('add', scope.model[i]);
+        }
+
+        var prev = scope.model.slice();
+        scope.$watch("model", function() {
+          var added = scope.model.filter(function(i) {return prev.indexOf(i) === -1;}),
+              removed = prev.filter(function(i) {return scope.model.indexOf(i) === -1;}),
+              same = scope.model.filter(function(i) {return prev.indexOf(i) > -1;}),
+              i;
+          prev = scope.model.slice();
+
+          for (i = 0; i < removed.length; i++) {
+            select.tagsinput('remove', removed[i]);
+          }
+
+          select.tagsinput('refresh');
+
+          for (i = 0; i < added.length; i++) {
+            select.tagsinput('add', added[i]);
+          }
+        }, true);
       });
     }
   };
