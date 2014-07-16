acee1589d67cacf414dcfb31ca3f0d3583b34841
Tim Schlechter <timschlechter@gmail.com>
2014-07-16 21:50:11 +0200

    Created 0.3.10 release build

--- dist/bootstrap-tagsinput.js
+++ dist/bootstrap-tagsinput.js
@@ -218,7 +218,7 @@
 
     /**
      * Assembly value by retrieving the value of each item, and set it on the
-     * element. 
+     * element.
      */
     pushVal: function() {
       var self = this,
@@ -296,6 +296,9 @@
       }
 
       self.$container.on('click', $.proxy(function(event) {
+        if (! self.$element.attr('disabled')) {
+          self.$input.removeAttr('disabled');
+        }
         self.$input.focus();
       }, self));
 
@@ -307,6 +310,11 @@
       self.$container.on('keydown', 'input', $.proxy(function(event) {
         var $input = $(event.target),
             $inputWrapper = self.findInputWrapper();
+
+        if (self.$element.attr('disabled')) {
+          self.$input.attr('disabled', 'disabled');
+          return;
+        }
 
         switch (event.which) {
           // BACKSPACE
@@ -363,6 +371,9 @@
 
       // Remove icon clicked
       self.$container.on('click', '[data-role=remove]', $.proxy(function(event) {
+        if (self.$element.attr('disabled')) {
+          return;
+        }
         self.remove($(event.target).closest('.tag').data('item'));
       }, self));
 
@@ -394,7 +405,7 @@
     },
 
     /**
-     * Sets focus on the tagsinput 
+     * Sets focus on the tagsinput
      */
     focus: function() {
       this.$input.focus();
@@ -462,9 +473,9 @@
   };
 
   $.fn.tagsinput.Constructor = TagsInput;
-  
+
   /**
-   * Most options support both a string or number as well as a function as 
+   * Most options support both a string or number as well as a function as
    * option value. This function makes sure that the option with the given
    * key in the given options is wrapped in a function
    */
