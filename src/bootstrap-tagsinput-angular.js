4462cea6e8edb2f7048ff3332f655410c70c1c4a
André Lourenço <andrel@lacunasoftware.com>
2014-09-10 17:18:38 -0300

    Added scope.$apply() calls after adding/removing items to make sure the model gets updated immediately.

--- src/bootstrap-tagsinput-angular.js
+++ src/bootstrap-tagsinput-angular.js
@@ -48,14 +48,18 @@ angular.module('bootstrap-tagsinput', [])
         }
 
         select.on('itemAdded', function(event) {
-          if (scope.model.indexOf(event.item) === -1)
+          if (scope.model.indexOf(event.item) === -1) {
             scope.model.push(event.item);
+			scope.$apply();
+		  }
         });
 
         select.on('itemRemoved', function(event) {
           var idx = scope.model.indexOf(event.item);
-          if (idx !== -1)
+          if (idx !== -1) {
             scope.model.splice(idx, 1);
+			scope.$apply();
+		  }
         });
 
         // create a shallow copy of model's current state, needed to determine
