70ee861c2fa92b473c9b0196859746e4490a2b51
A---- <A----@users.noreply.github.com>
2015-04-21 18:04:42 +0200

    Regression fix. Input value was still displayed after typeahead selection.

--- src/bootstrap-tagsinput.js
+++ src/bootstrap-tagsinput.js
@@ -302,7 +302,7 @@
           },
           updater: function (text) {
             self.add(this.map[text]);
-            return this.map[text];
+            return '';
           },
           matcher: function (text) {
             return (text.toLowerCase().indexOf(this.query.trim().toLowerCase()) !== -1);
