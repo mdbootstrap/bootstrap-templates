0ff64cf3c3aaea4c7be036683e013e4c5180638d
perecedero <perecedero@gmail.com>
2015-05-11 10:35:57 -0300

    refactored object items tests

--- test/bootstrap-tagsinput/input_with_object_items.tests.js
+++ test/bootstrap-tagsinput/input_with_object_items.tests.js
@@ -1,34 +1,15 @@
 describe("bootstrap-tagsinput", function() {
-
   describe("with objects as items", function() {
+
     testTagsInput('<input type="text" />', function() {
-      it("adding a item should throw an exception", function() {
+      it("Adding an item without itemValue option set should throw an exception", function() {
         var element = this.$element;
         expect(function() { element.tagsinput('add', {}); }).toThrow("Can't add objects when itemValue option is not set");
       });
     });
 
-    testTagsInput('<input type="text" />', { itemValue: function(item) { return item.value; } }, function() {
-      describe("adding an item", function() {
-        var item;
-        beforeEach(function() {
-          item = { value: 1 };
-          this.$element.tagsinput('add', item);
-        });
-        it("'items' should return the item", function() {
-          expect(this.$element.tagsinput('items')[0]).toBe(item);
-        });
-        it("val() should return the item's value", function() {
-          expect(this.$element.val()).toBe("1");
-        });
-        it("tag's text should be the item's value", function() {
-          expect($('.tag', this.$sandbox).text()).toBe("1");
-        });
-      });
-    });
-
     testTagsInput('<input type="text" />', { itemValue: 'value' }, function() {
-      describe("adding an item", function() {
+      describe("Adding an item", function() { //, indicating object's property name in itemValue
         var item;
         beforeEach(function() {
           item = { value: 1 };
@@ -49,61 +30,80 @@ describe("bootstrap-tagsinput", function() {
       });
     });
 
-    testTagsInput('<input type="text" />', { itemValue: function(item) { return item.value; }, itemText: function(item) { return item.text; } }, function() {
-      describe("adding an item", function() {
+    testTagsInput('<input type="text" />', { itemValue: function(item) { return item.id; } }, function() {
+      describe("Adding an item using a function as itemValue", function() {
+        var item;
+        beforeEach(function() {
+          item = { id: 1 };
+          this.$element.tagsinput('add', item);
+        });
+        it("val() should return the item's value", function() {
+          expect(this.$element.val()).toBe("1");
+        });
+      });
+    });
+
+    testTagsInput('<input type="text" />', { itemValue: 'value', itemText: 'text' }, function() {
+      describe("Adding an item including a tag text using itemText option as object's property name", function() {
+
         var item = { value: 1, text: 'some' };
         beforeEach(function() {
           this.$element.tagsinput('add', item);
         });
-        it("'items' should return the item", function() {
-          expect(this.$element.tagsinput('items')[0]).toBe(item);
-        });
-        it("val() should return the item's value", function() {
-          expect(this.$element.val()).toBe("1");
-        });
+
         it("tag's text should be the item's text", function() {
           expect($('.tag', this.$sandbox).text()).toBe("some");
         });
+      });
+    });
 
-        describe("change item and invoke 'refesh'", function() {
-          beforeEach(function() {
-            item.text = 'changed';
-            this.$element.tagsinput('refresh');
-          });
+    testTagsInput('<input type="text" />', { itemValue: 'value', itemText: function(item) { return item.name; } }, function() {
+      describe("Adding an item including a tag text using itemText option as function", function() {
 
-          it("should update tags's text", function() {
-            expect($('.tag', this.$sandbox).text()).toBe('changed');
-          });
-          it("tag should still have remove button", function() {
-            expect($('[data-role=remove]', this.$sandbox)[0]).not.toBeUndefined();
-          });
+        var item = { value: 1, name: 'Mr, Doe' };
+        beforeEach(function() {
+          this.$element.tagsinput('add', item);
+        });
+
+        it("tag's text should be the item's text", function() {
+          expect($('.tag', this.$sandbox).text()).toBe("Mr, Doe");
         });
       });
     });
-	
-    testTagsInput('<input type="text" />', { itemValue: function(item) { return item.value; }, itemText: function(item) { return item.text; }, itemTitle: function(item) { return item.title; } }, function() {
-	  describe("adding an item with a title", function() {
+
+    testTagsInput('<input type="text" />', { itemValue: 'value', itemTitle: function(item) { return item.title; } }, function() {
+      describe("Adding an item with a title", function() {
         var item;
+
         beforeEach(function() {
           item = { value: 1, text: 'one', title: 'number one' };
           this.$element.tagsinput('add', item);
         });
-        it("'items' should return the item", function() {
-          expect(this.$element.tagsinput('items')[0]).toBe(item);
+
+        it("tag's title should be the item's title", function() {
+          expect($('.tag', this.$sandbox).attr('title')).toBe("number one");
         });
-        it("'items' should returns exactly 1 item", function() {
-          expect(this.$element.tagsinput('items').length).toBe(1);
+      });
+    });
+
+    testTagsInput('<input type="text" />', { itemValue: 'value', itemText: 'text' }, function() {
+      describe("Change item and invoke refresh to apply changes" , function() {
+
+        var item = { value: 1, text: 'some' };
+        beforeEach(function() {
+          this.$element.tagsinput('add', item);
+          item.text = 'changed';
+          this.$element.tagsinput('refresh');
         });
-        it("val() should return the item's value", function() {
-          expect(this.$element.val()).toBe("1");
+
+        it("should update tags's text", function() {
+          expect($('.tag', this.$sandbox).text()).toBe('changed');
         });
-        it("tag's text should be the item's value", function() {
-          expect($('.tag', this.$sandbox).text()).toBe("one");
+        it("tag should still have remove button", function() {
+          expect($('[data-role=remove]', this.$sandbox)[0]).not.toBeUndefined();
         });
-		it("tag's title should be the item's title", function() {
-		  expect($('.tag', this.$sandbox).attr('title')).toBe("number one");
-		});
-	  });
-	});
+      });
+    });
+
   });
 });
