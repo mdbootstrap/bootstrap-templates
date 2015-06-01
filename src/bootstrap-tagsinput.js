12948ec7d96a9d8bb1b2a8758837b8cd76d110b4
Nikhil Mohan <nikhilmkumar@gmail.com>
2015-06-01 13:30:47 +0530

    keep original bootstrap input style
    keep original bootstrap input style

--- src/bootstrap-tagsinput.js
+++ src/bootstrap-tagsinput.js
@@ -42,7 +42,7 @@
     this.inputSize = Math.max(1, this.placeholderText.length);
 
     this.$container = $('<div class="bootstrap-tagsinput"></div>');
-    this.$input = $('<input type="text" placeholder="' + this.placeholderText + '"/>').appendTo(this.$container);
+    this.$input = $('<input type="text" class="form-control" placeholder="' + this.placeholderText + '"/>').appendTo(this.$container);
 
     this.$element.before(this.$container);
 
