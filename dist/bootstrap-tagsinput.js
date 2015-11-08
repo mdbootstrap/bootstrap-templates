17b51b629b997e64e32a29c098b718343732d985
Luckner Jr. Jean-Baptiste <lucknerjb@gmail.com>
2015-11-08 18:46:50 -0500

    Closes #399 - Checking for raw and uri-encoded form of tag in select options before appending

--- dist/bootstrap-tagsinput.js
+++ dist/bootstrap-tagsinput.js
@@ -136,8 +136,14 @@
       self.findInputWrapper().before($tag);
       $tag.after(' ');
 
+      // Check to see if the tag exists in its raw or uri-encoded form
+      var optionExists = (
+        $('option[value="' + encodeURIComponent(itemValue) + '"]', self.$element).length ||
+        $('option[value="' + htmlEncode(itemValue) + '"]', self.$element).length
+      );
+
       // add <option /> if item represents a value not present in one of the <select />'s options
-      if (self.isSelect && !$('option[value="' + encodeURIComponent(itemValue) + '"]',self.$element)[0]) {
+      if (self.isSelect && !optionExists) {
         var $option = $('<option selected>' + htmlEncode(itemText) + '</option>');
         $option.data('item', item);
         $option.attr('value', itemValue);
