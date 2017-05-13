30631b749e9abbdd9171060810d157739e2a4b4c
OlehBoiko <minace002@gmail.com>
2017-05-13 15:34:14 +0300

    Added check case sensitive

--- src/bootstrap-tagsinput.js
+++ src/bootstrap-tagsinput.js
@@ -28,7 +28,8 @@
     },
     trimValue: false,
     allowDuplicates: false,
-    triggerChange: true
+    triggerChange: true,
+    caseSensitive: true
   };
 
   /**
@@ -109,8 +110,8 @@
           tagClass = self.options.tagClass(item),
           itemTitle = self.options.itemTitle(item);
 
-      // Ignore items allready added
-      var existing = $.grep(self.itemsArray, function(item) { return self.options.itemValue(item) === itemValue; } )[0];
+       // Ignore items already added
+      var existing = $.grep(self.itemsArray, function(item) { return self.options.caseSensitive ?  self.options.itemValue(item) === itemValue : self.options.itemValue(item).toLowerCase() === itemValue.toLowerCase();} )[0];
       if (existing && !self.options.allowDuplicates) {
         // Invoke onTagExists
         if (self.options.onTagExists) {
