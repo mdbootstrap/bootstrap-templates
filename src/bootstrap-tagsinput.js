9864be898e7bcfc086df415f5c55228293d9fcd6
suri <dberrueta@expand.com.uy>
2018-03-14 16:11:13 -0300

    Fix input auto size small issue. See issue #538

--- src/bootstrap-tagsinput.js
+++ src/bootstrap-tagsinput.js
@@ -456,7 +456,7 @@
         var textLength = $input.val().length,
             wordSpace = Math.ceil(textLength / 5),
             size = textLength + wordSpace + 1;
-        $input.attr('size', Math.max(this.inputSize, $input.val().length));
+        $input.attr('size', Math.max(this.inputSize, size));
       }, self));
 
       self.$container.on('keypress', 'input', $.proxy(function(event) {
@@ -486,7 +486,7 @@
          var textLength = $input.val().length,
             wordSpace = Math.ceil(textLength / 5),
             size = textLength + wordSpace + 1;
-         $input.attr('size', Math.max(this.inputSize, $input.val().length));
+         $input.attr('size', Math.max(this.inputSize, size));
       }, self));
 
       // Remove icon clicked
